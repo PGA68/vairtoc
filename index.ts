f06c6ccefc16dbc9a247ba4e7d56126d967bbfcb
PGA68 <agdars@gmail.com>
2023-09-30 18:24:49 +0100

    tele

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+//
+/*   Since You Needed DOM, if it true   */
+
+import { version } from "process";
+
+const fs =  import.meta.file;
+
+const dir = import.meta.dir;
+
+
+
+console.log("Hello via Bun!", { Bun: version });
